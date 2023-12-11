bdaca6f049866f86dd3bc08cf651909045198c09
Vasil Rusinov <114298909+rusinovvasil@users.noreply.github.com>
2023-12-11 15:15:22 +0100

    Beginner Challenges
    Add some beginner challenges

---
+++ Very-Easy-Challenges/additionPlusOne.js
@@ -0,0 +1,10 @@
+function addition(num) {
+    let result = num + 1;
+    return result;
+}
+
+addition(0); // ➞ 1
+
+addition(9); // ➞ 10
+
+addition(-3); // ➞ -2
---
+++ Very-Easy-Challenges/convert.js
@@ -0,0 +1,10 @@
+function convert(minutes) {
+    let minutesToSeconds = minutes * 60;
+    return minutesToSeconds;
+}
+
+convert(5); // ➞ 300
+
+convert(3); // ➞ 180
+
+convert(2); // ➞ 120
