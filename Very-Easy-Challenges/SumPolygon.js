62f4b3d7b169d17df2a1f1bb83e320f511e2158f
Vasil Rusinov <114298909+rusinovvasil@users.noreply.github.com>
2023-12-15 13:05:30 +0100

    Very Easy Challenges
    Add some very easy tasks to build math skills

---
+++ Very-Easy-Challenges/SumPolygon.js
@@ -0,0 +1,11 @@
+function sumPolygon(n) {
+
+    let degrees = (n - 2) * 180;
+    console.log(degrees);
+}
+
+sumPolygon(3); // ➞ 180
+
+sumPolygon(4); // ➞ 360
+
+sumPolygon(6); // ➞ 720
---
+++ Very-Easy-Challenges/isEqual-or-Not.js
@@ -0,0 +1,14 @@
+function lessThanOrEqualToZero(num) {
+
+    if (num <= 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+lessThanOrEqualToZero(5); // ➞ false
+
+lessThanOrEqualToZero(0); // ➞ true
+
+lessThanOrEqualToZero(-2); // ➞ true
