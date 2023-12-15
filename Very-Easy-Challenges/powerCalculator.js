d1c319da51e0343a736d6251b7a4396026dd5875
Vasil Rusinov <114298909+rusinovvasil@users.noreply.github.com>
2023-12-15 12:23:20 +0100

    powerCalculator
    Add a task "powerCalculator"

---
+++ Very-Easy-Challenges/powerCalculator.js
@@ -0,0 +1,11 @@
+function circuitPower(voltage, current) {
+
+    return voltage * current;
+
+}
+
+
+circuitPower(230, 10); // -> 2300
+circuitPower(110, 3); // -> 330
+circuitPower(480, 20); // -> 9600
+
