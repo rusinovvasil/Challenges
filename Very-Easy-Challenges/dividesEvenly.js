8ac9bdd09093f754a362fa4ccb7215ad56c218e3
Vasil Rusinov <114298909+rusinovvasil@users.noreply.github.com>
2023-12-17 15:57:33 +0000

    Very Easy Challenges
    Add some very easy tasks to build strong math and coding skills

---
+++ Very-Easy-Challenges/dividesEvenly.js
@@ -0,0 +1,10 @@
+function dividesEvenly(a, b) {
+
+    console.log(a % b === 0 ? true : false);
+}
+
+dividesEvenly(98, 7); // ➞ true
+// 98/7 = 14
+
+dividesEvenly(85, 4); // ➞ false
+// 85/4 = 21.25
---
+++ Very-Easy-Challenges/recursionLengthOfStr.js
@@ -0,0 +1,12 @@
+function length(str) {
+
+    console.log(str.length);
+}
+
+length("apple"); // ➞ 5
+
+length("make"); // ➞ 4
+
+length("a"); // ➞ 1
+
+length(""); // ➞ 0
