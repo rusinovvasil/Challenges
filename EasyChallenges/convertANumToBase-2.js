d3f3beb79368aa529ba674821c7ba4de2a2b9b65
Vasil Rusinov <114298909+rusinovvasil@users.noreply.github.com>
2023-12-21 14:19:18 +0100

    Easy Challenges
    Add a solution for the task - How to convert a decimal to binary

---
+++ EasyChallenges/convertANumToBase-2.js
@@ -0,0 +1,13 @@
+function binary(num) {
+
+    return num.toString(2);
+}
+
+console.log(binary(1)); // ➞ "1"
+// 1*1 = 1
+
+console.log(binary(5)); // ➞ "101"
+// 1*1 + 1*4 = 5
+
+console.log(binary(10)); // ➞ "1010"
+// 1*2 + 1*8 = 10
